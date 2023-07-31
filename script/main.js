a17b931ea4a5446e35c0d9eeb3e76d870b0c170e
sweetlyra01 <binibingsweetlyra@gmail.com>
2023-07-31 22:26:07 +0800

    made-changes

--- script/main.js
+++ script/main.js
@@ -1,42 +1,27 @@
-const select = document.querySelectorAll(".currency");
-const btn = document.getElementById("btn");
-const num = document.getElementById("num");
-const ans = document.getElementById("ans");
+document.getElementById("submit-btn").onclick = function(){
 
-fetch("https://api.frankfurter.app/currencies")
-  .then((data) => data.json())
-  .then((data) => {
-    display(data);
-  });
+  let temp;
 
-function display(data) {
-  const entries = Object.entries(data);
-  for (var i = 0; i < entries.length; i++) {
-    select[0].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
-    select[1].innerHTML += `<option value="${entries[i][0]}">${entries[i][0]}</option>`;
+  if(document.getElementById("celcius-btn").checked){
+      temp = document.getElementById("temp-to-convert-box").value;
+      temp = Number(temp);
+      temp = toCelsius(temp);
+      document.getElementById("tempLabel").innerHTML = temp + "°C";
+  }
+  else if(document.getElementById("fahrenheit-btn").checked){
+      temp = document.getElementById("temp-to-convert-box").value;
+      temp = Number(temp);
+      temp = toFahrenheit(temp);
+      document.getElementById("tempLabel").innerHTML = temp + "°F";
+  }
+  else{
+      document.getElementById(tempLabel).innerHTML = "select a unit"
   }
 }
 
-btn.addEventListener("click", () => {
-  let currency1 = select[0].value;
-  let currency2 = select[1].value;
-  let value = num.value;
-
-  if (currency1 != currency2) {
-    convert(currency1, currency2, value);
-  } else {
-    alert("Choose Different Currencies !!!");
-  }
-});
-
-function convert(currency1, currency2, value) {
-  const host = "api.frankfurter.app";
-  fetch(
-    `https://${host}/latest?amount=${value}&from=${currency1}&to=${currency2}`
-  )
-    .then((val) => val.json())
-    .then((val) => {
-      console.log(Object.values(val.rates)[0]);
-      ans.value = Object.values(val.rates)[0];
-    });
+function toCelsius(temp){
+  return (temp - 32) * (5/9);
+}
+function toFahrenheit(temp){
+  return  temp * 9 / 5 + 32;
 }
